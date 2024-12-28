ef7cd862bb3cc00df6c5f22fd032b920e24c0b38
Christian Berkman <christianberkman@gmail.com>
2024-12-28 20:43:45 +0300

    include assets in repo

---
+++ app/Publishers/JQueryPublisher.php
@@ -0,0 +1,21 @@
+<?php
+namespace App\Publishers;
+use CodeIgniter\Publisher\Publisher;
+
+/**
+ * Publishes the composer package christianberkma/bookf-format to app/Libraries
+ */
+
+ class JQueryPublisher extends Publisher
+ {
+    
+    protected $destination = FCPATH . 'assets/jquery';
+
+    public function publish(): bool
+    {
+        return 
+            $this
+                ->addUri('https://code.jquery.com/jquery-3.7.1.min.js')
+                ->merge(true);
+    }
+ }
