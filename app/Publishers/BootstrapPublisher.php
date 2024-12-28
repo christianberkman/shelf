61768f00856d5563bbacbb24b05e9275112da1bc
Christian Berkman <christianberkman@gmail.com>
2024-12-28 16:39:26 +0300

    install twitter/bootstrap

---
+++ app/Publishers/BootstrapPublisher.php
@@ -0,0 +1,23 @@
+<?php
+namespace App\Publishers;
+use CodeIgniter\Publisher\Publisher;
+
+/**
+ * Publishes the composer package christianberkma/bookf-format to app/Libraries
+ */
+
+ class BootstrapPublisher extends Publisher
+ {
+    
+    protected $source = VENDORPATH . 'twitter/bootstrap';
+    
+    protected $destination = FCPATH . 'assets/bootstrap';
+
+    public function publish(): bool
+    {
+        return $this
+            ->addPath('dist/')
+            ->retainPattern('*.min.*')
+            ->merge(true);
+    }
+ }
