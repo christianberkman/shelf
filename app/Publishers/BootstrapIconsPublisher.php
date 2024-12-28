8b0543b1eef498f23b0770be6605b97faa25d101
Christian Berkman <christianberkman@gmail.com>
2024-12-28 16:44:27 +0300

    install twbs/bootstrap-icons

---
+++ app/Publishers/BootstrapIconsPublisher.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Publishers;
+use CodeIgniter\Publisher\Publisher;
+
+/**
+ * Publishes the composer package christianberkma/bookf-format to app/Libraries
+ */
+
+ class BootstrapIconsPublisher extends Publisher
+ {
+    
+    protected $source = VENDORPATH . 'twbs/bootstrap-icons';
+    
+    protected $destination = FCPATH . 'assets/bootstrap-icons';
+
+    public function publish(): bool
+    {
+        return $this
+            ->addPath('font/')
+            ->merge(true);
+    }
+ }
