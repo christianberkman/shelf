78e36704d00edbf47a42c925c6cd9d8869f24ec3
Christian Berkman <christianberkman@gmail.com>
2025-01-07 23:53:13 +0300

    feat: [sections] dropdown required option

--- app/Views/sections/dropdown.php
+++ app/Views/sections/dropdown.php
@@ -1,5 +1,5 @@
-<select name="section_id" id="sectionId" class="form-select">
-
+<select name="section_id" id="sectionId" class="form-select" <?=($required ?? true) ? 'required' : ''; ?>>
+    <option value="">Select...</option>
     <?php foreach(sectionModel()->orderBy('name')->findAll() as $section): ?>
         <option value="<?=$section->section_id; ?>" <?=(($selected ?? null) === $section->section_id ? 'selected' : ''); ?>><?=$section->name; ?></option>
     <?php endforeach; ?>
